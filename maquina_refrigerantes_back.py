46ea8fa3d08fd19647a54207bff7b83393394355
yasmin bonilha <bonilhayaya@gmail.com>
2024-10-02 16:51:00 -0300

    commit inicial

---
+++ maquina_refrigerantes_back.py
@@ -0,0 +1,31 @@
+# conjunto finito de estados
+S = ['S0', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8']
+
+# alfabeto finito de entradas
+I = ['m25', 'm50', 'm100', 'b']
+
+# alfabeto finito de saídas
+O = ['t25', 't50', 't100', 'r', 'n']
+
+# estado inicial
+S0 = S[0]
+
+# função de transição de estados (não sei como fazer)
+T = [[[], []], ]
+
+valor = 0.0
+troco = 0.0
+estado = S0
+
+# função que executa o autômato
+def reconhecer(valor_entrada):
+
+
+
+# # testes
+# if __name__ == '__main__':
+#     reconhecer('0100') # aceita
+#     reconhecer('010') #  rejeita
+
+
+
